f841ad51f4b9aaf094c7b5a5f174f57c08e5aa4d
Dolly Setton <drs22@columbia.edu>
2018-03-03 00:29:05 -0500

    early version of assignment 5 added, with linear scale

---
+++ Assignments/Assignment5/js/sketch.js
@@ -0,0 +1,63 @@
+//***** Global variables ***** //
+// var moviesTable;
+// var topY = 50;
+// var bottomY = 500;
+// var leftX = 50;
+// var rightX = 700;
+// var textLeft = 30;
+var neighborhood = ["Gramercy","Soho","Upper East","Upper West","West Village"];
+// var number = ["0","10000", "20000", "30000", "40000", "50000"];
+var number = [0,10000, 20000, 30000, 40000, 50000];
+
+
+
+//***** Preload function ***** //
+function preload(){
+  NeighborhoodTable = loadTable('Data/try.csv', 'csv', 'header');
+  // console.log('Done loading table...');
+}
+
+// ***** Setup function ***** //
+function setup(){
+  createCanvas(800, 800);
+  // textAlign(RIGHT, CENTER);
+  textSize(12);
+  textFont('Roboto');
+  console.log('Setup complete...');
+  print(NeighborhhoodTable.getRowCount() + ' rows loaded...');
+  print(NeighborhoodTable.getColumnCount() + ' columns loaded...');
+  noLoop();
+}
+
+// ***** Draw function ***** //
+function draw(){
+  background(255);
+  fill(0);
+  for (var i = 0; i < 6; i++) {
+    // noStroke();
+    text(number[i], 20, map(number[i], 0, 50000, 750, 0));
+    print(number[i]);
+    // stroke(200);
+    // line(textLeft + 10, map(i, 0, 10, bottomY, topY), rightX + 10, map(i, 0, 10, bottomY, topY));
+  }
+  // noStroke();
+  for (var i = 0; i < NeighborhoodTable.getRowCount(); i++) {
+    // var date = moviesTable.getString(i, 'release_date').split('-')[0];
+    // var year = parseInt(date);
+    // var yearPosition = map(year, 1916, 2017, leftX, rightX);
+    // var scorePosition = map(moviesTable.getNum(i, 'vote_average'), 0, 10, bottomY, topY);
+    fill(150);
+    var q3 = NeighborhoodTable.getNum(i, 'Q3');
+    var q3map = map(q3, 0, 50000, 750, 0);
+    var q1 = NeighborhoodTable.getNum(i, 'Q1');
+    var q1map = map(q1, 0, 50000,750,0);
+    var median = NeighborhoodTable.getNum(i, 'Median');
+    var medianmap = map(median, 0, 50000,750,0);
+    line(i*100+70, medianmap, i*100+140, medianmap);
+    rect(i*100+80, q3map, 50, (q1map-q3map));
+    text(neighborhood[i], i*100+80, 790);
+  }
+  line(60, 780, 800, 780);
+  line(60, 100, 60, 780);
+  text("abcdfsdfs",400, 790);
+}
