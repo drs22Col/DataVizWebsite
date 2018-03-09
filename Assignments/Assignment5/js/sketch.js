afb635f16470864f1685430b678099f9bbf9f622
Dolly Setton <drs22@columbia.edu>
2018-03-09 03:08:24 -0500

    Added color, adjusted axes, adjusted boxes, added dots to assignment 5

--- Assignments/Assignment5/js/sketch.js
+++ Assignments/Assignment5/js/sketch.js
@@ -7,13 +7,13 @@
 // var textLeft = 30;
 var neighborhood = ["1","2","3","4","5"];
 // var number = ["0","10000", "20000", "30000", "40000", "50000"];
-var number = [0,10000, 20000, 30000, 40000, 50000];
+var number = [0,10000, 20000, 30000, 40000, 50000, 60000];
 
 
 
 //***** Preload function ***** //
 function preload(){
-  moviesTable = loadTable('Data/try.csv', 'csv', 'header');
+  moviesTable = loadTable('../Assignment5/Data/try.csv', 'csv', 'header');
   // console.log('Done loading table...');
 }
 
@@ -21,7 +21,7 @@ function preload(){
 function setup(){
   createCanvas(800, 800);
   // textAlign(RIGHT, CENTER);
-  textSize(12);
+  textSize(18);
   textFont('Roboto');
   console.log('Setup complete...');
   print(moviesTable.getRowCount() + ' rows loaded...');
@@ -31,15 +31,15 @@ function setup(){
 
 // ***** Draw function ***** //
 function draw(){
-  background(255);
+  background(20,0,185);
   fill(0);
-  textSize(18);
-  for (var i = 0; i < 6; i++) {
+  for (var i = 0; i < 7; i++) {
     // noStroke();
-    text(number[i], 20, map(number[i], 0, 50000, 750, 0));
+    text(number[i], 20, map(number[i], 0, 60000, 750, 40));
     print(number[i]);
+    textSize(18);
     // stroke(200);
-    // line(textLeft + 10, map(i, 0, 10, bottomY, topY), rightX + 10, map(i, 0, 10, bottomY, topY));
+    //line(textLeft + 10, map(i, 0, 10, bottomY, topY), rightX + 10, map(i, 0, 10, bottomY, topY));
   }
   // noStroke();
   for (var i = 0; i < moviesTable.getRowCount(); i++) {
@@ -47,18 +47,35 @@ function draw(){
     // var year = parseInt(date);
     // var yearPosition = map(year, 1916, 2017, leftX, rightX);
     // var scorePosition = map(moviesTable.getNum(i, 'vote_average'), 0, 10, bottomY, topY);
-    fill(150);
+    fill(200,120,0);
     var q3 = moviesTable.getNum(i, 'Q3');
-    var q3map = map(q3, 0, 50000, 750, 0);
-    var q1 = NeighborhoodTable.getNum(i, 'Q1');
-    var q1map = map(q1, 0, 50000,750,0);
+    var q3map = map(q3, 0, 60000, 750, 0);
+    //
+    var q1 = moviesTable.getNum(i, 'Q1');
+    var q1map = map(q1, 0, 60000, 750, 0);
+    //
     var median = moviesTable.getNum(i, 'Median');
-    var medianmap = map(median, 0, 50000,750,0);
-    line(i*100+70, medianmap, i*100+140, medianmap);
-    rect(i*100+80, q3map, 50, (q1map-q3map));
-    text(neighborhood[i], i*100+80, 790);
+    var medianmap = map(median, 0, 60000, 750, 0);
+    //
+    rect(i*130+100, q3map, 100, (q1map-q3map));
+
+
+    line(i*130+90, medianmap, i*130+210, medianmap);
+    
+    fill(0);
+    text(neighborhood[i], i*100+120, 790);
+    //making the top outlier
+    var max = moviesTable.getNum(i, 'Max');
+    var maxmap = map(max, 0, 60000, 750,0);
+    fill(255);
+    ellipse(i*130+150, maxmap, 15, 15);
+
   }
-  line(60, 780, 800, 780);
-  line(60, 100, 60, 780);
+  line(60, 760, 800, 760);
+  line(80, 20, 80, 780);
+
+
   text("abcdfsdfs",400, 790);
+
+
 }
