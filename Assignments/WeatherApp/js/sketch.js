3fbb0fd8331b3426b82748d7650278c52b12ed4a
Dolly Setton <drs22@columbia.edu>
2018-03-11 04:31:22 -0400

    styled home page: changed background color and title color, placement of text, added images and placed them, added margins around bio text, etc.

--- Assignments/WeatherApp/js/sketch.js
+++ Assignments/WeatherApp/js/sketch.js
@@ -1,21 +1,47 @@
 // **** Global Variables ***** //
-var apiKey = 'your api key here';
-var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+var apiKey = 'ccac5f3796d194d4a15e7a3fcc8ccda6';var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
 var weatherData;
-
-// **** Preload Function **** //
-function preload(){
-  var request = baseURL + 'New York' + '&apikey=' + apiKey;
-  weatherData = loadJSON(request);
-  print(weatherData);
-}
+var button;
+var cityInput;
+var description = '';
+var temperature = 0;
+var humidity = 0;
+var pressure = 0;
 
 // **** Setup Function ****** //
 function setup(){
   createCanvas(800, 800);
+  button = select('#submit');
+  cityInput = select('#city');
+  button.mousePressed(queryAPI);
   noLoop();
 }
 
+// **** Query API Function *** //
+function queryAPI(){
+  var request = baseURL + cityInput.value() + '&apikey=' + apiKey;
+  loadJSON(request, getWeatherData);
+}
+
+function getWeatherData(apiData){
+  weatherData = apiData;
+  description = weatherData.weather[0].description;
+  temperature = weatherData.main.temp;
+  humidity = weatherData.main.humidity;
+  pressure = weatherData.main.pressure;
+  print(weatherData);
+  redraw();
+}
+
+// **** Draw Function **** //
 function draw(){
   background(225);
+  if (weatherData){
+    text('The current weather for ' + cityInput.value() + ' is:', 50, 50);
+    text(description, 80, 70);
+    text(temperature + ' F', 80, 90);
+    text(humidity + '% humidity', 80, 110);
+    text(pressure + ' hPa (pressure)', 80, 130);
+  }
 }
+ 
--- Assignments/WeatherApp/sketch.js
+++ Assignments/WeatherApp/sketch.js
@@ -1,27 +1,47 @@
 // **** Global Variables ***** //
-var apiKey = 'ccac5f3796d194d4a15e7a3fcc8ccda6';
-var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+var apiKey = 'b51108f7b72966280bcdfff37e9092b3';var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
 var weatherData;
-var city = 'New York';
-
-// **** Preload Function **** //
-function preload(){
-  var request = baseURL + city + '&apikey=' + apikey;
-  weatherData = loadJSON(request);
-  print(weatherData);
-}
+var button;
+var cityInput;
+var description = '';
+var temperature = 0;
+var humidity = 0;
+var pressure = 0;
 
 // **** Setup Function ****** //
 function setup(){
-  createCanvas(500, 500);
-  background(100);
-  noLoop()
- 
+  createCanvas(800, 800);
+  button = select('#submit');
+  cityInput = select('#city');
+  button.mousePressed(queryAPI);
+  noLoop();
 }
 
-function draw(){
-	var myWeatherData = weatherData.wind;
-	var speed = myWeatherData.speed
-	var pressure = myWeatherData.pressure
-  
+// **** Query API Function *** //
+function queryAPI(){
+  var request = baseURL + cityInput.value() + '&apikey=' + apiKey;
+  loadJSON(request, getWeatherData);
 }
+
+function getWeatherData(apiData){
+  weatherData = apiData;
+  description = weatherData.weather[0].description;
+  temperature = weatherData.main.temp;
+  humidity = weatherData.main.humidity;
+  pressure = weatherData.main.pressure;
+  print(weatherData);
+  redraw();
+}
+
+// **** Draw Function **** //
+function draw(){
+  background(225);
+  if (weatherData){
+    text('The current weather for ' + cityInput.value() + ' is:', 50, 50);
+    text(description, 80, 70);
+    text(temperature + ' F', 80, 90);
+    text(humidity + '% humidity', 80, 110);
+    text(pressure + ' hPa (pressure)', 80, 130);
+  }
+}
+ 
