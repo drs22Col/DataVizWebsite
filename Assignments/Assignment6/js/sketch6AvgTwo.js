f27decadcb933a5840fe9e6ddc3e43493ae6f186
Dolly Setton <drs22@columbia.edu>
2018-04-05 16:23:37 -0400

    tweak

--- Assignments/Assignment6/js/sketch6AvgTwo.js
+++ Assignments/Assignment6/js/sketch6AvgTwo.js
@@ -19,7 +19,7 @@ var properties = [];
  //***** Preload function ***** //
 function preload(){
     AvgAssessedTable = loadTable('./Data/ZipCode4.csv', 'csv', 'header');
-    
+    // console.log('Done loading table...');
  }
 
  // ***** Setup function ***** //
